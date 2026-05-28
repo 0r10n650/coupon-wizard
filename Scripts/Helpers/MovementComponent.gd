cdf2ec5fa1fce29a202d49ffeb23615b9c7d1a93
Jordan Rand <jorrand@gmail.com>
2026-05-28 17:49:14 -0400

    fix inputs and readme

--- Scripts/Helpers/MovementComponent.gd
+++ Scripts/Helpers/MovementComponent.gd
@@ -12,21 +12,21 @@ var next_direction = Vector3.ZERO
 
 func move(delta):
 	
-	if Input.is_action_pressed("Forward"):
+	if Input.is_action_pressed("move_forward"):
 		if current_speed < max_speed:
 			current_speed += acceleration * delta
 	
-	if Input.is_action_pressed("Backward"):
+	if Input.is_action_pressed("move_backward"):
 		if current_speed > 0:
 			current_speed -= acceleration * 4 * delta
 	
-	if Input.is_action_pressed("Left"):
+	if Input.is_action_pressed("move_left"):
 		parent.rotate_y(rotation_speed * delta)
 	
-	if Input.is_action_pressed("Right"):
+	if Input.is_action_pressed("move_right"):
 		parent.rotate_y(-rotation_speed * delta)
 	
-	if Input.is_action_just_pressed("Jump") and parent.is_on_floor():
+	if Input.is_action_just_pressed("move_jump") and parent.is_on_floor():
 		parent.velocity.y = 6
 	
 	if not parent.is_on_floor():
