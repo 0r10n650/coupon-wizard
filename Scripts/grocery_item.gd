6e406f14f2fa2ed6eb6c6b6d3d978f19dd29ef29
0r10n650 <orion4cooler@gmail.com>
2026-05-28 15:27:12 -0600

    Created Grocery item

---
+++ Scripts/grocery_item.gd
@@ -0,0 +1,6 @@
+extends Node
+class_name item_data
+
+@export var item_name : String
+@export var price: float
+ 
--- Scripts/shopping_wizard.gd
+++ Scripts/shopping_wizard.gd
@@ -5,13 +5,25 @@ class_name ShoppingWizard
 @onready var wizard = $WizardMesh
 @onready var camera = $Camera3D
 @onready var leaningC = $LeaningComponent
+@onready var leftCast = $LeftCast
+@onready var rightCast = $RightCast
 
 func _physics_process(delta):
 	movementC.move(delta)
 	move_and_slide()
 
+func attempt_grab_left():
+	grab("left")
 
+func attempt_grab_right():
+	grab("right")
 
+func grab(direction):
+	if direction == "left":
+		pass
+	else:
+		pass
+	
 func _on_left_lean_button_mouse_entered():
 	leaningC.lean(-1)
 
