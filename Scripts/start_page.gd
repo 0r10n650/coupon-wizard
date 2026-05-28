46bc08a429a2a5968c10069480e9a04fa3f46718
0r10n650 <orion4cooler@gmail.com>
2026-05-28 15:56:12 -0600

    Created Start Screen

---
+++ Scripts/start_page.gd
@@ -0,0 +1,12 @@
+extends Node2D
+class_name  start_page
+
+const jordo_scene = preload("res://coupon_game/coupon_game_test.tscn")
+const o_scene = preload("res://Scenes/shopping.tscn")
+
+func _on_jordos_button_pressed():
+	get_tree().change_scene_to_packed(jordo_scene)
+
+
+func _on_o_button_pressed():
+	get_tree().change_scene_to_packed(o_scene)
