ab13f6cddd01b5aed656ecf235ef93d36164c688
0r10n650 <orion4cooler@gmail.com>
2026-05-28 19:17:49 -0600

    Added button for other demo

--- Scripts/start_page.gd
+++ Scripts/start_page.gd
@@ -3,6 +3,7 @@ class_name  start_page
 
 const jordo_scene = preload("res://coupon_game/coupon_game_test.tscn")
 const o_scene = preload("res://Scenes/shopping.tscn")
+const carpy_scene = preload("res://tests/shoppingfordummies.tscn")
 
 func _on_jordos_button_pressed():
 	get_tree().change_scene_to_packed(jordo_scene)
@@ -10,3 +11,7 @@ func _on_jordos_button_pressed():
 
 func _on_o_button_pressed():
 	get_tree().change_scene_to_packed(o_scene)
+
+
+func _on_carpy_demo_pressed():
+	get_tree().change_scene_to_packed(carpy_scene)
