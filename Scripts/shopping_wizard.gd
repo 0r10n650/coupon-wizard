9d11374334b0027af720b289ca0896706ce780ca
Jordan Rand <jorrand@gmail.com>
2026-06-04 16:34:16 -0400

    update movement

--- Scripts/shopping_wizard.gd
+++ Scripts/shopping_wizard.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 class_name ShoppingWizard
 
-@onready var movementC = $MovementComponent
+@onready var movementC = $DriftingMovementComponent
 @onready var wizard = $WizardMesh
 @onready var camera = $Camera3D
 @onready var leaningC = $LeaningComponent
